28b2b7769b404a7ae9eba6ee7823e4b0eb14db4a
PsychoHacks <jamie.v732@gmail.com>
2019-03-12 11:46:40 +0000

    Routing complete time to add the rest of the pages

---
+++ src/app/bluetooth-connect/bluetooth-connect.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bluetooth-connect',
+  templateUrl: './bluetooth-connect.page.html',
+  styleUrls: ['./bluetooth-connect.page.scss'],
+})
+export class BluetoothConnectPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
